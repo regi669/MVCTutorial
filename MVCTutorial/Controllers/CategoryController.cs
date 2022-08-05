2307c509431feddf3c01deb091f4efb8862eeed6
Szymon <regicsmaster@gmail.com>
2022-08-05 19:36:33 +0200

    Server side validation

--- MVCTutorial/Controllers/CategoryController.cs
+++ MVCTutorial/Controllers/CategoryController.cs
@@ -18,4 +18,23 @@ public class CategoryController : Controller
         IEnumerable<Category> categories = _dbContext.Categories.ToList();
         return View(categories);
     }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.Add(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View(category);
+    }
 }
--- MVCTutorial/Entities/Category.cs
+++ MVCTutorial/Entities/Category.cs
@@ -6,7 +6,8 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public int? DisplayOrder { get; set; }
-    public DateTime? CreatedDateTime { get; set; } = DateTime.Now;
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
